9ac1658a7ae25ae5768ed413a91a6f38ab8fe8de
Bharath Padmaraju <bharath.k.padmaraju@gmail.com>
2022-01-23 17:42:37 -0800

    feat(s3): upload json api data to s3

--- mockweatherservice/main.py
+++ mockweatherservice/main.py
@@ -1,4 +1,10 @@
 import requests
+import os
+import json
+
+import boto3
+
+from datetime import datetime
 from typing import Optional
 from fastapi import FastAPI
 
@@ -15,7 +21,25 @@ def read_root():
 @app.get("/data/2.5/weather")
 def read_item():
     #return {"item_id": item_id, "q": q}
+    session = boto3.Session(
+        aws_access_key_id=os.environ['MY_AWS_ACCESS_ID'],
+        aws_secret_access_key=os.environ['MY_AWS_SECRET_KEY']
+    )
+    s3 = session.resource('s3')
     r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=corvallis&appid=a99f1dbe7d7e7d5b6ce85970a31da042')
+    now = datetime.now()
+    #date_time = now.strftime("%m-%d-%Y_%H:%M:%S")
+
+    object = s3.Object('weatherapi-cs561-bharath', f"weather.json")
+    result = object.put(Body=(bytes(json.dumps(r.json()).encode('UTF-8'))))
+
+    res = result.get('ResponseMetadata')
+
+    if res.get('HTTPStatusCode') == 200:
+        print('File Uploaded Successfully')
+    else:
+        print('File Not Uploaded')
+
     return r.json()
 
 def server():
